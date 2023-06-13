ce4355c3dd0f6dcf3d6336576a78d2261741b2a8
suijiyuan <suijiyuan@outlook.com>
2023-06-13 17:48:17 +0800

    feat: optimize no-captcha.py

--- app/no-captcha.py
+++ app/no-captcha.py
@@ -1,14 +1,14 @@
 import pytesseract
 import PIL
-import matplotlib.pyplot as plt
+import matplotlib.pyplot
 
 # 读取验证码图片
-image = PIL.Image.open('captcha.png')
+image = PIL.Image.open('app/captcha.png')
 
 # 将图片转为灰度图像
 image = image.convert('L')
-# plt.imshow(image)
-# plt.show()
+# matplotlib.pyplot.imshow(image)
+# matplotlib.pyplot.show()
 
 # 对图像进行二值化处理
 threshold = 127
@@ -19,8 +19,8 @@ for i in range(256):
     else:
         table.append(1)
 image = image.point(table, '1')
-# plt.imshow(image)
-# plt.show()
+# matplotlib.pyplot.imshow(image)
+# matplotlib.pyplot.show()
 
 # 使用pytesseract进行识别
 code = pytesseract.image_to_string(
